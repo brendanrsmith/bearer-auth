78081dffc06fff71b05dde138c602c53f8b2ddc9
Brendan Smith <brendan5320@gmail.com>
2021-04-27 19:11:17 -0700

    readme updates

--- src/auth/models/users.js
+++ src/auth/models/users.js
@@ -18,7 +18,7 @@ users.virtual('token').get(function () {
   let tokenObject = {
     username: this.username,
   }
-  return jwt.sign(tokenObject, SECRET);
+  return jwt.sign(tokenObject, SECRET, { expiresIn: '15m' } );
 });
 
 users.pre('save', async function () {
